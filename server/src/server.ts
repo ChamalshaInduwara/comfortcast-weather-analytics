4a5eaef72eee3ca34a3877385f866f285f1c1f5b
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-08-26 20:02:46 +0530

    Import city service for city code handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import { getWeatherByCityId } from "./services/weather.service.js";
+import { getCityCodes } from "./services/city.service.js";
 
 dotenv.config();
 
